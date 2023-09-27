5544585ca7ecc67f3331658dec275fe91b9b9ba7
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-09-27 05:44:12 +0200

    created views for User, Lesson, Course, added urls, added .env.sample

---
+++ users/urls.py
@@ -0,0 +1,18 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path
+from users.apps import UsersConfig
+from users.views import UserViewSet
+# from users.views import UserListAPIView, UserRetrieveAPIView, UserCreateAPIView, UserUpdateAPIView, UserDestroyAPIView
+
+name_app = UsersConfig.name
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    # path('list/', UserListAPIView.as_view(), name='users_list'),
+    # path('detail/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_detail'),
+    # path('create/', UserCreateAPIView.as_view(), name='user_create'),
+    # path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
+    # path('delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user_delete'),
+] + router.urls
--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+# class UserListAPIView(ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+#
+# class UserRetrieveAPIView(RetrieveAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+#
+# class UserCreateAPIView(CreateAPIView):
+#     serializer_class = UserSerializer
+#
+#
+# class UserUpdateAPIView(UpdateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#
+#
+# class UserDestroyAPIView(DestroyAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
