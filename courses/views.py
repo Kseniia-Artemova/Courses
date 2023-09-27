25a0d0e550558116b3fbd8fc0a7c6e99ff56d95f
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-09-27 04:58:11 +0200

    created serializers for Course and Lesson, added views for Course

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from courses.models import Course
+from courses.serializers import CourseSerializer
+
+
+class CourseListAPIView(ListAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseRetrieveAPIView(RetrieveAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseCreateAPIView(CreateAPIView):
+    serializer_class = CourseSerializer
+
+
+class CourseUpdateAPIView(UpdateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseDestroyAPIView(DestroyAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
