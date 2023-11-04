1c4c019b2f86d3c7f2611b65f0a993d30178dbee
Ksu <kseniyaartemova@yandex.ru>
2023-11-04 13:24:33 +0100

    created .env.docker file, chenged docker-compose.yaml, server runs

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import logging
 from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
@@ -98,6 +99,7 @@ DATABASES = {
     }
 }
 
+logging.critical(DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
